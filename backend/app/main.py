2dcf61019caba105a136e8c779e93322a79c6b8d
ayush061107 <ayushodanwal101@gmail.com>
2026-07-22 18:48:40 +0530

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from app.database import Base, engine 
 from app.routes import auth, product, compare, recommend
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app=FastAPI(title="DecisionFlow API",version="1.0.0")
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(product.router)
